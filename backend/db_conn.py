f8567c18684ca887d8b73671a3db9e870fd7b6e5
Abraham Dagnew <Ahaymero@gmail.com>
2026-08-06 13:50:40 -0400

    Add default database connection values

--- backend/db_conn.py
+++ backend/db_conn.py
@@ -12,11 +12,11 @@ def get_connection():
     """
 
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME")
+        host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASS", ""),
+        database=os.getenv("DB_NAME", "portfolio")
     )
 
 
